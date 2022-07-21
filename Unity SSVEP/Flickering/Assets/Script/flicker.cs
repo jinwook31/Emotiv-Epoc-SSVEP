3edf292da5a6d0eb81f87ab00a310b4398b55a46
jinwook31 <jinwook.kim31@gmail.com>
2022-07-21 18:04:46 +0900

    project structure

---
+++ Unity SSVEP/Flickering/Assets/Script/flicker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flicker : MonoBehaviour{
+    private GameObject targ;
+
+    // Start is called before the first frame update
+    void Start(){
+        targ = GameObject.FindGameObjectWithTag("target");
+
+        InvokeRepeating("setRed", 0, 0.1f);
+        InvokeRepeating("setWhite", 0.05f, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    private void setRed(){
+        var targRenderer = targ.GetComponent<Renderer>();
+        targRenderer.material.SetColor("_Color", Color.red);
+    }
+
+    private void setWhite(){
+        var targRenderer = targ.GetComponent<Renderer>();
+        targRenderer.material.SetColor("_Color", Color.white);
+    }
+}
